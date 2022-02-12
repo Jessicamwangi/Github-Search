23fe884cea4c6928841f201248c6cf031612f2ad
Jessica <jessicamwangi933@gmail.com>
2022-02-12 20:52:50 +0300

    updated my git hub api

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -8,15 +8,15 @@ import { map } from 'rxjs/operators';
 export class ProfileService {
 
   private username: string | undefined;
-  private clientid = '';
-  private clientsecret = '';
+  private clientid = ' Iv1.371be666824d7a51';
+  private clientsecret = '53388e8c6cdd6a556e1e25e2e452ffc87352a125';
 
   constructor(private http:HttpClient) {
     console.log("service is now ready!");
     this.username = 'Jessicamwangi';
    }
    getProfileInfo () {
-    return this.http.get("https//api.github.com/users/" + this.username )
+    return this.http.get("https://api.github.com/users/" + this.username + " ?client_id= " + this.clientid + " &client_secret= " + this.clientsecret)
     .pipe(map((res:any) => res.json()));
    }
 }
