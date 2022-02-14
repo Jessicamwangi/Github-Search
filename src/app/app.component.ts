022426f44473a5e429ecc7a74955fd60f3200e43
Jessica <jessicamwangi933@gmail.com>
2022-02-14 08:26:30 +0300

    added imports to app component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './profile-service/profile.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
+ 
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProfileService]
 })
 export class AppComponent {
   title = 'GithubSearch';
