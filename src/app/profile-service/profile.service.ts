e76e7da76a74fa01cb19775555dbd2c513f2404f
Jessica <jessicamwangi933@gmail.com>
2022-02-14 06:03:16 +0300

    replaced the resjson with result

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -17,6 +17,6 @@ export class ProfileService {
    }
    getProfileInfo () {
     return this.http.get("https://api.github.com/users/" + this.username + " ?client_id= " + this.clientid + " &client_secret= " + this.clientsecret)
-    .pipe(map((res:any) => res.json()));
+    .pipe(map(result =>result));
    }
 }
