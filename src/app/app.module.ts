81d69b788980106d013f38898b5c759240bd13d2
Jessica <jessicamwangi933@gmail.com>
2022-02-14 08:22:04 +0300

    imported forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { ProfileService } from './profile-service/profile.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,9 +19,10 @@ import { ProfileComponent } from './profile/profile.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [ProfileService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
